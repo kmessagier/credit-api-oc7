3daa9bbd12b04e4faf1ed2410b0e7c59d0d8897d
kmessagier <89636815+kmessagier@users.noreply.github.com>
2021-12-17 17:15:04 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,28 +1,19 @@
 
 from flask import Flask, jsonify
 import numpy as np
-
 import pandas as pd
 import lightgbm
 
-
-
-
-
-
-
-app = Flask(__name__)
-
-
 # loading the trained model
-pickle_in = open('lgbm.pkl', 'rb')
+pickle_in = open('./lgbm.pkl', 'rb')
 lgbm = pickle.load(pickle_in)
 print('Le modèle a été importé')
 
 # loading data
-data_client = pd.read_csv('mini_data_test.csv')
-data_client_without_id = pd.read_csv('data_test_mini_without_id.csv')
+data_client = pd.read_csv('./mini_data_test.csv')
+data_client_without_id = pd.read_csv('./data_test_mini_without_id.csv')
 
+app = Flask(__name__)
 @app.route('/api/client2')
 def client2():
     return '<h1>TEST API FONCTIONNELLE</h1>'
