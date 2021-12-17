fa611f866366dbde8be5733925e31c56802c8e69
kmessagier <89636815+kmessagier@users.noreply.github.com>
2021-12-17 17:08:41 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,6 +14,14 @@ import lightgbm
 app = Flask(__name__)
 
 
+# loading the trained model
+pickle_in = open('lgbm.pkl', 'rb')
+lgbm = pickle.load(pickle_in)
+print('Le modèle a été importé')
+
+# loading data
+data_client = pd.read_csv('mini_data_test.csv')
+data_client_without_id = pd.read_csv('data_test_mini_without_id.csv')
 
 @app.route('/api/client2')
 def client2():
