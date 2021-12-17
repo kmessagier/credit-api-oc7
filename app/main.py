21d9fb48c95a59fb4f3932f908e2cc5fd0048dc4
kmessagier <89636815+kmessagier@users.noreply.github.com>
2021-12-17 17:40:24 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ import pandas as pd
 import lightgbm
 
 
-# loading data
-data_client = pd.read_csv('./mini_data_test.csv')
-data_client_without_id = pd.read_csv('./data_test_mini_without_id.csv')
 
 app = Flask(__name__)
 @app.route('/api/client2')
@@ -15,5 +12,8 @@ def client2():
     return '<h1>TEST API FONCTIONNELLE</h1>'
 
 
+# loading data
+data_client = pd.read_csv('./mini_data_test.csv')
+data_client_without_id = pd.read_csv('./data_test_mini_without_id.csv')
 
 
