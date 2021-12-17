bbcd3fc506431ade0e85e75c39df555374d26254
kmessagier <89636815+kmessagier@users.noreply.github.com>
2021-12-17 16:39:51 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,16 +6,7 @@ import pickle
 import pandas as pd
 import lightgbm
 
-# loading the trained model
 
-
-pickle_in = open('lgbm.pkl', 'rb')
-lgbm = pickle.load(pickle_in)
-print('Le modèle a été importé')
-
-# loading data
-data_client = pd.read_csv('mini_data_test.csv')
-data_client_without_id = pd.read_csv('data_test_mini_without_id.csv')
 
 
 
