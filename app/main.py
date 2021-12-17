3b090f655b03ac40f68cc45b6ee147fc4e20f371
kmessagier <89636815+kmessagier@users.noreply.github.com>
2021-12-17 17:49:58 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,3 +13,28 @@ def client2():
 
 
 
+@app.route('/api/client/<id_client>')
+def client(id_client):
+    print("id_client:<"+ id_client+">")
+    id_client = float(id_client)
+
+    # Nouvelle donnée à interpréter
+   # index de la ligne du client à partir de son identifiant SK_ID_CURR
+    index = data_client.index[data_client['SK_ID_CURR'] == id_client].tolist()[0]
+
+
+
+    # Calcul des probabilités d'appartenance aux classes 0 et 1
+    y_proba = lgbm.predict_proba(data_client_without_id.iloc[index,1:].array.reshape(1, -1))
+
+
+
+
+    dico = {}
+    dico["proba0"] = str(np.round(y_proba[0][0], 2))
+    dico["proba1"] = str(np.round(y_proba[0][1], 2))
+
+    return jsonify(dico)
+
+
+
