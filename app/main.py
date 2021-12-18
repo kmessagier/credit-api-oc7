38fb9f8f803900f728315b799db9f635a4c5cf2a
kmessagier <89636815+kmessagier@users.noreply.github.com>
2021-12-18 11:45:47 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ try:
       pickle_in = open('lgbm.pkl', 'rb')
       lgbm = pickle.load(pickle_in)
 except Exception as e :
-      Print(e)
+      print('ERROR :' +str(e))
     
 
 print('Le modèle a été importé')
@@ -27,32 +27,32 @@ print('Les données ont été importées')
 app = Flask(__name__)
 @app.route('/api/client2')
 def client2():
-    return '<h1>TEST API FONCTIONNELLE</h1>'
+      return '<h1>TEST API FONCTIONNELLE</h1>'
 
 
 
 @app.route('/api/client/<id_client>')
 def client(id_client):
-    print("id_client:<"+ id_client+">")
-    id_client = float(id_client)
+      print("id_client:<"+ id_client+">")
+      id_client = float(id_client)
 
     # Nouvelle donnée à interpréter
    # index de la ligne du client à partir de son identifiant SK_ID_CURR
-    index = data_client.index[data_client['SK_ID_CURR'] == id_client].tolist()[0]
+      index = data_client.index[data_client['SK_ID_CURR'] == id_client].tolist()[0]
 
 
 
     # Calcul des probabilités d'appartenance aux classes 0 et 1
-    y_proba = lgbm.predict_proba(data_client_without_id.iloc[index,1:].array.reshape(1, -1))
+      y_proba = lgbm.predict_proba(data_client_without_id.iloc[index,1:].array.reshape(1, -1))
 
 
 
 
-    dico = {}
-    dico["proba0"] = str(np.round(y_proba[0][0], 2))
-    dico["proba1"] = str(np.round(y_proba[0][1], 2))
+      dico = {}
+      dico["proba0"] = str(np.round(y_proba[0][0], 2))
+      dico["proba1"] = str(np.round(y_proba[0][1], 2))
 
-    return jsonify(dico)
+      return jsonify(dico)
 
 
 
