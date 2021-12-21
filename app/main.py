2a39a631299a33c5ac80ce29958aac1cf65ed97e
kmessagier <89636815+kmessagier@users.noreply.github.com>
2021-12-21 02:17:25 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, jsonify
 import numpy as np
 import pickle
@@ -7,13 +6,13 @@ import pandas as pd
 
 
 # loading the trained model
-pickle_in = open('data/lgbm.pkl', 'rb')
+pickle_in = open('app/lgbm.pkl', 'rb')
 lgbm = pickle.load(pickle_in)
 print('Le modèle a été importé')
 
 # loading data
-data_client = pd.read_csv('data/mini_data_test.csv')
-data_client_without_id = pd.read_csv('data/data_test_mini_without_id.csv')
+data_client = pd.read_csv('app/mini_data_test.csv')
+data_client_without_id = pd.read_csv('app/data_test_mini_without_id.csv')
 
 
 print('Les données ont été importées')
@@ -21,6 +20,9 @@ print('Les données ont été importées')
 app = Flask(__name__)
 
 
+@app.route('/api/client2')
+def client():
+    print("API FONCTIONNELLE")
 
 @app.route('/api/client/<id_client>')
 def client(id_client):
@@ -29,14 +31,16 @@ def client(id_client):
 
     # Nouvelle donnée à interpréter
    # index de la ligne du client à partir de son identifiant SK_ID_CURR
-    index = data_client.index[data_client['SK_ID_CURR'] == id_client].tolist()[0]
-
+    #index = data_client.index[data_client['SK_ID_CURR'] == id_client].tolist()[0]
+    list_xtest = data_client.columns.tolist()  # Création d'une liste récupérant les features
+    # st.write(list_xtest)
+    list_xtest_without_id = [e for e in list_xtest if e not in 'SK_ID_CURR']  # Modification de la liste sans SK_ID_CURR pour lancer la future prédiction
 
 
     # Calcul des probabilités d'appartenance aux classes 0 et 1
-    y_proba = lgbm.predict_proba(data_client_without_id.iloc[index, 1:].array.reshape(1, -1))
+    #y_proba = lgbm.predict_proba(data_client_without_id.iloc[index, 1:].array.reshape(1, -1))
 
-
+    y_proba = lgbm.predict_proba(data_client.loc[data_client['SK_ID_CURR']==id_client, list_xtest_without_id:].array.reshape(1, -1))
 
 
     dico = {}
@@ -48,5 +52,4 @@ def client(id_client):
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
 
