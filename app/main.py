793d63b90570b6c26b1552f58df7e0dcfcd92d73
kmessagier <89636815+kmessagier@users.noreply.github.com>
2021-12-21 01:46:34 +0100

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -13,14 +13,10 @@ print('Le modèle a été importé')
 
 # loading data
 data_client = pd.read_csv('data/mini_data_test.csv')
-list_xtest = data_client.columns.tolist() # Création d'une liste récupérant les features
-#st.write(list_xtest)
-list_xtest_without_id = [e for e in list_xtest if e not in 'SK_ID_CURR'] #Modification de la liste sans SK_ID_CURR pour lancer la future prédiction
-#st.write(list_xtest_without_id)
-# Nouvelle donnée à interpréter
+data_client_without_id = pd.read_csv('data/data_test_mini_without_id.csv')
 
 
-
+print('Les données ont été importées')
 
 app = Flask(__name__)
 
@@ -30,19 +26,15 @@ app = Flask(__name__)
 def client(id_client):
     print("id_client:<"+ id_client+">")
     id_client = int(id_client)
-    X_try = data_client.iloc[:, :130].to_numpy()
-    ix = data_client.index[data_client['SK_ID_CURR'] == id_client].tolist()[0]
-    idx = X_try[ix, :]
-    print(ix)
-    print(idx)
+
     # Nouvelle donnée à interpréter
    # index de la ligne du client à partir de son identifiant SK_ID_CURR
-    #index = data_client.index[data_client['SK_ID_CURR'] == id_client].tolist()[0]
+    index = data_client.index[data_client['SK_ID_CURR'] == id_client].tolist()[0]
 
 
 
     # Calcul des probabilités d'appartenance aux classes 0 et 1
-    y_proba = lgbm.predict_proba(data_client_without_id.iloc[ix,1:].array.reshape(1, -1))
+    y_proba = lgbm.predict_proba(data_client_without_id.iloc[index,:130].array.reshape(1, -1))
 
 
 
