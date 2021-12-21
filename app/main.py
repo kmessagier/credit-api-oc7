42c182d82f319a591956ff055d648d2b73353004
kmessagier <89636815+kmessagier@users.noreply.github.com>
2021-12-21 01:28:52 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ def client2():
 @app.route('/api/client/<id_client>')
 def client(id_client):
     print("id_client:<"+ id_client+">")
-    id_client = float(id_client)
+    id_client = int(id_client)
 
     # Nouvelle donnée à interpréter
    # index de la ligne du client à partir de son identifiant SK_ID_CURR
